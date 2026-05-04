3f6805426113872011b0610ae7084672d92899cb
miguelgfierro <miguelgfierro@users.noreply.github.com>
2026-05-04 15:29:07 +0200

    feat(factory): scaffold action_runtime package

---
+++ tests/unit/factory/action_runtime/test_imports.py
@@ -0,0 +1,22 @@
+# Copyright 2026 Firefly Software Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Smoke import test for the factory action_runtime package."""
+from __future__ import annotations
+
+
+def test_action_runtime_package_imports() -> None:
+    import fireflyframework_agentic.factory.action_runtime as rt
+
+    assert rt is not None
